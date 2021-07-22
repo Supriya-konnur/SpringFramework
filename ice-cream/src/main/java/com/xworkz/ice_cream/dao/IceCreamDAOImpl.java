7770cb0133f651dbaceee7acbc287ef488a685b8
Supriya-konnur <73068126+Supriya-konnur@users.noreply.github.com>
2021-07-22 17:29:09 +0530

    pusing code

--- ice-cream/src/main/java/com/xworkz/ice_cream/dao/IceCreamDAOImpl.java
+++ ice-cream/src/main/java/com/xworkz/ice_cream/dao/IceCreamDAOImpl.java
@@ -1,4 +1,4 @@
-package com.xworkz.ice_cream.dto;
+package com.xworkz.ice_cream.dao;
 
 import java.util.List;
 
@@ -9,7 +9,7 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.xworkz.ice_cream.dao.IceCreamDAO;
+import com.xworkz.ice_cream.dto.IceCreamDTO;
 @Component
 public class IceCreamDAOImpl implements IceCreamDAO{
 
